a416bfd5df3e31e5470838ff456d9d1008998cd1
Dhruv_Anand <ananddhruv475@gmail.com>
2024-06-14 02:55:10 +0530

    Cleanup

--- src/app.js
+++ src/app.js
@@ -28,15 +28,12 @@ import dashboardRouter from "./routes/dashboard.routes.js"
 import postRouter from "./routes/post.routes.js"
 
 //routes declaration
-app.use("/api/v1/healthcheck", healthcheckRouter)
+
 app.use("/api/v1/users", userRouter)
-app.use("/api/v1/tweets", tweetRouter)
+
 app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
 app.use("/api/v1/posts", postRouter )
 
 // http://localhost:8000/api/v1/users/register
